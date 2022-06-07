bcabfaeb14f3de2a7b9ce7ad3c8ccf721fb1a9db
Ilya Volkov <illvolkov@bk.ru>
2022-06-07 21:48:01 +0500

    Create GeneratedThread and WorkerThread

--- ChipSolderingWithThreads.playground/Contents.swift
+++ ChipSolderingWithThreads.playground/Contents.swift
@@ -22,3 +22,83 @@ public struct Chip {
         sleep(UInt32(soderingTime))
     }
 }
+
+var time = 20
+
+//MARK: - GeneratedThread
+
+class GeneratedThread: Thread {
+            
+    override func main() {
+        name = "GeneratedThread"
+        RunLoop.current.add(timer, forMode: .default)
+        RunLoop.current.run()
+    }
+    
+    let timer = Timer(timeInterval: 2, repeats: true) { timer in
+            
+        time -= 2
+        
+        if time < 0 {
+            timer.invalidate()
+            Thread.current.cancel()
+            print("----------------Generation finished\n")
+            print("Remaining chips in storage: \(chips)\n")
+            print("----------------GeneratedThread is cancelled? \(GeneratedThread.current.isCancelled)\n")
+        } else {
+            semaphoreAppend()
+        }
+    }
+}
+
+//MARK: - WorkerThread
+
+class WorkerThread: Thread {
+    
+    var chip: Chip?
+        
+    override func main() {
+        name = "WorkerThread"
+                                
+        while chips.isEmpty {
+            
+            Thread.sleep(forTimeInterval: 0.1)
+            
+            while !chips.isEmpty {
+                semaphorePop()
+                chip?.sodering()
+                print("The chip is soldered\n")
+            }
+        }
+    }
+}
+
+//MARK: - Stack
+
+var chips = [Chip]()
+
+let semaphore = DispatchSemaphore(value: 1)
+
+func semaphoreAppend() {
+            
+    semaphore.wait()
+    chips.append(Chip.make())
+    print("\(Thread.current.name ?? "") - Chip added: \(chips.last ?? Chip(chipType: .big))")
+    semaphore.signal()
+}
+
+func semaphorePop() -> Chip {
+    
+    semaphore.wait()
+    let index = chips.count - 1
+    print("\(Thread.current.name ?? "") - Chip taken: \(chips.last ?? Chip(chipType: .big))")
+    let poppedChip = chips.remove(at: index)
+    semaphore.signal()
+    return poppedChip
+}
+
+let generate = GeneratedThread()
+let worker = WorkerThread()
+
+generate.start()
+worker.start()
